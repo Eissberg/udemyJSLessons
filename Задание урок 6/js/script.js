18c14af46509e8f05db78de7ae89c6aa619f4c27
EiSSberG <jeka.chk@gmail.com>
2020-02-15 13:45:28 +0200

    15022020_homeTask6_remake

--- Задание урок 6/js/script.js
+++ Задание урок 6/js/script.js
@@ -1,18 +1,22 @@
 let items = prompt("Каково Ваше отношение к технике Apple?","");
-let menuElement = document.querySelector('.menu'),
-    menuPoint = document.getElementsByClassName('menu-item'),
-    text = document.getElementsByClassName('title'),
-    elements = document.getElementsByClassName('column'),
-    kill  = document.querySelector('.adv');
+let 
+    menu = document.querySelector('.menu'),
+    menuElements = document.querySelectorAll('.menu-item'),
+    menuIthem = document.createElement("li");
+    menuIthem.classList.add('menu-item');
+    menuIthem.textContent = 'Пятый пункт';
+    text = document.getElementById('title'),
+    elements = document.querySelectorAll('.column'),
+    elementForKill  = document.querySelector('.adv');
     forMessage = document.getElementById('prompt');
-
- menuElement.insertBefore(menuPoint[2], menuPoint[1]);  
- document.body.style.backgroundImage = 'url(../img/apple_true.jpg)';
-
- text[0].textContent= "Мы продаем только подлинную технику Apple"; 
-
- elements[1].removeChild(kill);
-
+  
+ document.body.style.backgroundImage = 'url(img/apple_true.jpg)';
  
+ menu.insertBefore(menuElements[2], menuElements[1]);
+ menu.appendChild(menuIthem);  
+ 
+ text.textContent= "Мы продаем только подлинную технику Apple"; 
+
+ elements[1].removeChild(elementForKill);
  forMessage.innerHTML = items;
- 
+
