c0bd5129dbbf24a7ab9ca84fb66c1582f86ff685
EiSSberG <jeka.chk@gmail.com>
2020-02-15 15:48:01 +0200

    15022020_homeTask_6_and_7

--- firstScript.js
+++ firstScript.js
@@ -1,4 +1,28 @@
 'use strict';
+let 
+    startButton = document.getElementById('start'),
+
+    budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItems = document.querySelectorAll('.expenses-item'),
+    buttonUTVexp = document.querySelectorAll('button')[0],
+    buttonUTVopt = document.querySelectorAll('button')[1],
+    buttonRASCH = document.querySelectorAll('button')[2],
+    optionalExpensesItems = document.querySelectorAll('.optionalexpenses-item'),
+    otherOptionalExpenses = document.querySelector('.choose-income'),
+    checkSavingsBool = document.querySelector('#checkbox'),
+    savingsSumm = document.querySelector('.choose-sum'),
+    savingsPercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.momth-value'),
+    dayValue = document.querySelector('.day-value');
+
 let money, time;
 
 function start() {
@@ -96,6 +120,6 @@ let appData = {
     }
 }
 
- for (let key in appData) {    // работа со свойствами обьекта
-     console.log("Наша программа включает в себя данные: "+ key + " имеет значение " + appData[key]);
- }
+//  for (let key in appData) {    // работа со свойствами обьекта
+//      console.log("Наша программа включает в себя данные: "+ key + " имеет значение " + appData[key]);
+//  }
